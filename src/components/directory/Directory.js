f0e670736d20ef9c34bf58082d82cd378205037e
Andy-Osei <andrew.owusu@novanest.net>
2021-11-23 02:20:44 +0000

    adding routes to menuitems

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -7,46 +7,41 @@ const sections = [
     title: "hats",
     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     id: 1,
-    linkUrl: "shop/hats",
+    linkUrl: "hats",
   },
   {
     title: "jackets",
     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     id: 2,
-    linkUrl: "shop/jackets",
+    linkUrl: "jackets",
   },
   {
     title: "sneakers",
     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     id: 3,
-    linkUrl: "shop/sneakers",
+    linkUrl: "sneakers",
   },
   {
     title: "womens",
     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     size: "large",
     id: 4,
-    linkUrl: "shop/womens",
+    linkUrl: "womens",
   },
   {
     title: "mens",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     size: "large",
     id: 5,
-    linkUrl: "shop/mens",
+    linkUrl: "mens",
   },
 ];
 
 const Directory = () => {
   return (
     <div className={styles["directory-menu"]}>
-      {sections.map((section) => (
-        <MenuItem
-          key={section.id}
-          title={section.title}
-          imageUrl={section.imageUrl}
-          size={section.size}
-        />
+      {sections.map(({ id, ...otherProps }) => (
+        <MenuItem key={id} {...otherProps} />
       ))}
     </div>
   );
