4c15cd8e9a28791b3396598dd707b8e82d5223b6
Andy-Osei <andrew.owusu@novanest.net>
2021-12-30 06:04:42 +0000

    feat-add items to cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addToCart } from "./cart-ustils";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     cartDropdown: false,
+    cartItems: [],
   },
   reducers: {
     toggleCartDropdown(state) {
       state.cartDropdown = !state.cartDropdown;
+    },
+    addItem(state, action) {
+      state.cartItems = addToCart(state.cartItems, action.payload);
     },
   },
 });
