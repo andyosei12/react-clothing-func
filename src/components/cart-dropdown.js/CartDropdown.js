c01a7cbbe25d86b0fabcaf261589274f8d1ec64f
Drew Osei <andrewowusu21@yahoo.com>
2021-12-30 11:56:13 +0000

    feat-displaying items in cart (#14)
    Co-authored-by: Andy-Osei <andrew.owusu@novanest.net>

--- src/components/cart-dropdown.js/CartDropdown.js
+++ src/components/cart-dropdown.js/CartDropdown.js
@@ -1,11 +1,19 @@
+import { useSelector } from "react-redux";
+import Cartitems from "../cart-items/CartItems";
 import CustomButton from "../custom-button/CustomButton";
 
 import "./CartDropdown.styles.scss";
 
 const CartDropdown = () => {
+  const cartItems = useSelector(({ cart: { cartItems } }) => cartItems);
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <Cartitems key={cartItem.id} items={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go to checkout</CustomButton>
     </div>
   );
