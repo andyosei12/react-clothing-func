e8a6543253e7b473304e3a2553047060674e4117
Andy-Osei <andrew.owusu@novanest.net>
2022-01-04 05:21:33 +0000

    bugfix-cart re-rendering

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { cartActions } from "../../store/cart-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { cartActions } from "../../store/cart/cart-slice";
+import { selectCartItemsCount } from "../../store/cart/cart-selectors";
 import { ReactComponent as CartBagIcon } from "../../assets/shopping-bag.svg";
 
 import "./CartIcon.styles.scss";
 
 function CartIcon() {
   const dispatch = useDispatch();
+  const itemCount = useSelector((state) => selectCartItemsCount(state));
 
   const cartDropdownHandler = () => {
     dispatch(cartActions.toggleCartDropdown());
@@ -14,7 +16,7 @@ function CartIcon() {
   return (
     <div className="cart-icon" onClick={cartDropdownHandler}>
       <CartBagIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./user-slice";
-import cartReducer from "./cart-slice";
+import cartReducer from "./cart/cart-slice";
 
 const store = configureStore({
   reducer: { user: userReducer, cart: cartReducer },
