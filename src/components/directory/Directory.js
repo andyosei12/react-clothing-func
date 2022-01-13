6ecbbb6685a47993ea815033f962304f65aa82b7
Andy-Osei <andrew.owusu@novanest.net>
2022-01-13 15:18:29 +0000

    moving shop data to redux

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,43 +1,46 @@
 import MenuItem from "../menu-item/MenuItem";
+import { useSelector } from "react-redux";
 
 import styles from "./Directory.module.scss";
+import { selectDirectorySections } from "../../store/directory/directory.selectors";
 
-const sections = [
-  {
-    title: "hats",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    id: 1,
-    linkUrl: "hats",
-  },
-  {
-    title: "jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    id: 2,
-    linkUrl: "jackets",
-  },
-  {
-    title: "sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    id: 3,
-    linkUrl: "sneakers",
-  },
-  {
-    title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    size: "large",
-    id: 4,
-    linkUrl: "womens",
-  },
-  {
-    title: "mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    size: "large",
-    id: 5,
-    linkUrl: "mens",
-  },
-];
+// const sections = [
+//   {
+//     title: "hats",
+//     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+//     id: 1,
+//     linkUrl: "hats",
+//   },
+//   {
+//     title: "jackets",
+//     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+//     id: 2,
+//     linkUrl: "jackets",
+//   },
+//   {
+//     title: "sneakers",
+//     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+//     id: 3,
+//     linkUrl: "sneakers",
+//   },
+//   {
+//     title: "womens",
+//     imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+//     size: "large",
+//     id: 4,
+//     linkUrl: "womens",
+//   },
+//   {
+//     title: "mens",
+//     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+//     size: "large",
+//     id: 5,
+//     linkUrl: "mens",
+//   },
+// ];
 
 const Directory = () => {
+  const sections = useSelector((state) => selectDirectorySections(state));
   return (
     <div className={styles["directory-menu"]}>
       {sections.map(({ id, ...otherProps }) => (
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -5,6 +5,8 @@ import storage from "redux-persist/lib/storage";
 //info: reducers
 import userReducer from "./user/user-slice";
 import cartReducer from "./cart/cart-slice";
+import directoryReducer from "./directory/directory.slice";
+import shopReducer from "./shop/shop.slice";
 
 const persistConfig = {
   key: "root",
@@ -15,6 +17,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
